44ba5444f4258996aec35e5c0c77da862491dbc6
yj <153605289+yjcstudy1@users.noreply.github.com>
2024-10-18 14:50:06 +0800

    Add files via upload

---
+++ url.js
@@ -0,0 +1,14 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 除最後一項外，其餘項目用逗號分隔
+ * 2. 網址請用引號包起來
+ */
+
+
+const urls = [
+	'https://docs.google.com/forms/d/e/1FAIpQLSfKYnaCRHEnOqsralLWG6whdzNfU07RPbTYRiXDk6BuxEUKuw/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSdIyeA1OaUcTcj3IHGPRVJqUrpxSCsTlBmGYMh2PkxHbMAfPg/viewform',
+        'https://docs.google.com/forms/d/e/1FAIpQLScGbqiQsVGSKufEB9ILCOCNN_HEAbjc4R1eLwAlhMJYimcqvA/viewform'
+];
